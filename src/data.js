1293f738625f4f6fac8d94c3e4f460e698441381
JK1320 <kaurjus1@gmail.com>
2022-09-16 12:05:47 +0100

    added array of categories in data file

--- src/data.js
+++ src/data.js
@@ -16,8 +16,26 @@ export const sliderItems = [
   {
     id: 3,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzuZWfIino6mB8tXE05kJAvCGKkXqN4UxKdw&usqp=CAU",
-    title: "LOUNGE WEAR",
+    title: "LOUNGEWEAR LOVE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
   },
 ];
+
+ export const categories = [
+   {
+     id: 1,
+     img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+     title: "SHIRT STYLE!",
+   },
+   {
+     id: 2,
+     img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+     title: "LOUNGEWEAR LOVE",
+   },
+   {
+     id: 3,
+     img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+     title: "LIGHT JACKETS",
+   },
+ ];
