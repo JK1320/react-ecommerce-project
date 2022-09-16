5b4485914f6dac353bf2475a7b94adee89ac4533
JK1320 <kaurjus1@gmail.com>
2022-09-16 11:11:37 +0100

    added title + description of product in slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {ArrowLeftOutlined, ArrowRightOutlined} from '@material-ui/icons';
+import { sliderItems } from '../data';
 
 // styled components - props used for Arrow to give direction as both (left & right) were at one place. 
 const Container = styled.div`
@@ -8,6 +9,7 @@ width: 100%;
 height: 100vh;
 display: flex;
 position: relative;
+overflow: hidden;
 `
 
 const Arrow = styled.div`
@@ -26,10 +28,13 @@ left: ${props=> props.direction === "left" && "10px"};
 right: ${props=> props.direction === "right" && "10px"};
 cursor: pointer;
 opacity: 0.5;
+z-index: 2;
 `
 
 const Wrapper = styled.div`
 height: 100%;
+display: flex;
+transform: translateX(0vw);
 `
 
 const Slide = styled.div`
@@ -37,6 +42,7 @@ display: flex;
 align-items: center;
 width: 100vw;
 height: 100vh;
+background-color: #${props => props.bg};
 `
 
 const ImgContainer = styled.div`
@@ -53,25 +59,57 @@ flex: 1;
 padding: 50px;
 `
 
+const Title = styled.h1`
+font-size: 70px;
+`
+
+const Desc = styled.p`
+margin: 50px 0px;
+font-size: 20px;
+font-weight: 500;
+letter-spacing: 3px;
+`
+
+const Button = styled.button`
+padding: 10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+`
+
 function Slider() {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {
+
+  }
+
   return (
     <Container>
-        <Arrow direction="left">
-            <ArrowLeftOutlined />
-        </Arrow>
-        <Wrapper>
-            <Slide>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg}>
             <ImgContainer>
-            <Image src="https://png.pngitem.com/pimgs/s/37-375764_midi-dress-download-transparent-png-image-midi-dress.png" />
+              <Image src={item.img} />
             </ImgContainer>
-            <InfoContainer></InfoContainer>
-            </Slide>
-        </Wrapper>
-        <Arrow direction="right">
-            <ArrowRightOutlined />
-        </Arrow>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>
+                {item.desc}
+              </Desc>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
-  )
+  );
 }
 
 export default Slider;
