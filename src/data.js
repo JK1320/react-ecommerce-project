f5878ddccc37362ee4aa6149add9c7aa51c25500
JK1320 <kaurjus1@gmail.com>
2022-09-16 11:12:22 +0100

    added array of objects for slider data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://png.pngitem.com/pimgs/s/37-375764_midi-dress-download-transparent-png-image-midi-dress.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://png.pngitem.com/pimgs/s/37-375764_midi-dress-download-transparent-png-image-midi-dress.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://png.pngitem.com/pimgs/s/37-375764_midi-dress-download-transparent-png-image-midi-dress.png",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+];
