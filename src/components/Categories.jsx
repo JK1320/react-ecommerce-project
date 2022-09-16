9047588446b6ff77adb463f133019c7aca432f94
JK1320 <kaurjus1@gmail.com>
2022-09-16 13:53:32 +0100

    import styled-components, categories from data file

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import { categories } from '../data';
+import CategoryItem from './CategoryItem';
 
-
+const Container = styled.div`
+display: flex;
+padding: 20px;
+`
 
 function Categories() {
   return (
-    <div>Categories</div>
+    <Container>
+        {categories.map(item => (
+            <CategoryItem item={item} />
+        ))}
+    </Container>
   )
 }
 
