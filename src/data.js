518643d4e9cc67d8ba36ba8b249d71c410fa626c
JK1320 <kaurjus1@gmail.com>
2022-09-16 11:43:09 +0100

    added new image address for id 2 & 3

--- src/data.js
+++ src/data.js
@@ -8,15 +8,15 @@ export const sliderItems = [
   },
   {
     id: 2,
-    img: "https://png.pngitem.com/pimgs/s/37-375764_midi-dress-download-transparent-png-image-midi-dress.png",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNEA1J5Txyp67ra2RhzprYBtmEiOOGLa4jgw&usqp=CAU",
     title: "AUTUMN COLLECTION",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://png.pngitem.com/pimgs/s/37-375764_midi-dress-download-transparent-png-image-midi-dress.png",
-    title: "POPULAR SALE",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzuZWfIino6mB8tXE05kJAvCGKkXqN4UxKdw&usqp=CAU",
+    title: "LOUNGE WEAR",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
   },
