646aaca755d05d2fa2a09a4d4d5643f935eecacf
JK1320 <kaurjus1@gmail.com>
2022-09-21 13:02:24 +0100

    added Select and Option styled components

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,20 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
+import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 const Container = styled.div`
 
 `
 const Title = styled.h1`
-
+margin: 20px;
 `
 
 const FilterContainer = styled.div`
-
+display: flex;
+justify-content: space-between;
 `
 const Filter = styled.div`
+margin: 20px;
+`
+const FilterText = styled.span`
+font-size: 20px;
+font-weight: 600;
+margin-right: 20px;
+`
+const Select = styled.select`
+padding: 10px;
+margin-right: 20px;
+`
+const Option = styled.option`
 
 `
+
 
 function ProductList() {
   return (
@@ -24,9 +41,38 @@ function ProductList() {
       <Navbar />
       <Title>Dresses</Title>
       <FilterContainer>
-        <Filter>filter</Filter>
-        <Filter>filter2</Filter>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>Color</Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+           <Select>
+            <Option disabled selected>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+          </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+           <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+          </Filter>
       </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
     </Container>
   );
 }
