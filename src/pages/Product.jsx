062c4fb4bc1e79441182bb62f3ab1b99c0aeb8a6
JK1320 <kaurjus1@gmail.com>
2022-09-21 13:48:43 +0100

    added styled components

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,16 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
+import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import Newsletter from '../components/Newsletter';
 
-const Container = styled.Container`
+const Container = styled.div`
+
+`
+const Wrapper = styled.div`
+padding: 50px;
+display: flex;
+`
+const ImgContainer = styled.div`
+flex: 1;
+`
+const Image = styled.img`
+width: 100%;
+height: 90vh;
+object-fit: cover;
+`
+const InfoContainer = styled.div`
+flex: 1;
+padding: 0 50px;
+`
+const Title = styled.h1`
+font-weight: 200;
+`
+const Desc = styled.p`
+margin: 20px 0;
+`
+const Price = styled.span`
+font-weight: 100;
+font-size: 40px;
+`
+const FilterContainer = styled.div`
 
 `
 
 function Product() {
   return (
     <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+            <Title>Denim Jumpsuit</Title>
+            <Desc> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+            tristique tortor pretium ut. Curabitur elit justo, consequat id
+            condimentum ac, volutpat ornare.</Desc>
+            <Price>$ 20</Price>
+            <FilterContainer>
 
+            </FilterContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
     </Container>
-  )
+  );
 }
 
 export default Product;
