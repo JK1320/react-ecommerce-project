673268fef0c7b4dfbca2d3ecce83053f6d2daf55
JK1320 <kaurjus1@gmail.com>
2022-09-25 11:27:13 +0100

    added media queries to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
 display: flex;
+ ${mobile({flexDirection: "column"})};
 `
 
 const Left = styled.div`
@@ -36,6 +38,7 @@ margin-right: 20px;
 const Center = styled.div`
 flex: 1;
 padding: 20px;
+ ${mobile({display: "none"})};
 `
 const Title = styled.h3`
 margin-bottom: 30px;
@@ -55,6 +58,7 @@ margin-bottom: 10px;
 const Right = styled.div`
 flex: 1;
 padding: 20px;
+ ${mobile({backgroundColor: "#fff8f8"})};
 `
 const ContactItem = styled.div`
 margin-bottom: 20px;
